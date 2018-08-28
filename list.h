f3d58337ef6e3e5bda74cad6a733b3c2fd126176
Joe-08 <33465109+Joe-08@users.noreply.github.com>
2018-08-28 18:17:14 -0500

    Add files via upload

--- list.h
+++ list.h
@@ -1,38 +1,158 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <iostream>
+#include "node.h"
+#include "iterator.h"
+
+using namespace std;
+
+template <typename T>
 class List {
-    struct Node {
-        int data;
-        struct Node* next;
-
-	void killSelf();
-    };
-
     private:
-        Node* head;
-        Node* tail;
+        Node<T>* head;
+        Node<T>* tail;
         int nodes;
 
-        void print_reverse(Node* head);
+        void print_reverse(Node<T>* head);
 
     public:
-        List();
+        List(){
+            nodes = 0;
+            head = NULL;
+            tail = NULL;
+        }
 
-        int front();
-        int back();
-        void push_front(int value);
-        void push_back(int value);
-        void pop_front();
-        void pop_back();
-        int get(int position);
-        bool empty();
-        int size();
-        void print();
-        void print_reverse();
-        void clear();
+        T front(){
+            if (!head){
+                throw "Lista vacia";
+            }
+            return head->data;
+        }
+
+        T back(){
+            if (!tail){
+                throw "Lista vacia";
+            }
+            return tail->data;
+        }
+
+        void push_front(T value){
+            Node<T>* temp = new Node<T>;
+            temp->data = value;
+            temp->next = nullptr;
+            nodes++;
+            if(nodes == 0){
+                head = temp;
+                tail = temp;
+            }
+            else{
+                temp->next = head;
+                head = temp;    
+            }
+            
+        }
+
+        void push_back(T value){
+            Node<T>* temp = new Node<T>;
+            temp->data = value;
+            temp->next = nullptr;
+            nodes++;
+            if (nodes == 0){
+                head = temp;
+                tail = temp;
+            }
+            else{
+                tail->next = temp;
+                tail = temp;
+            }
+        }
+        
+        void pop_front(){
+            if (!head)
+            {
+                throw "Lista vacia";
+                return;
+            }
+            Node<T> *temp = head;
+            head = head->next;
+            delete temp;
+        }
+
+        void pop_back(){
+            if (!head)
+            {
+                throw "Lista vacia";
+                return;
+            }
+            Node<T> *tail2 = tail;
+            Node<T> *temp = head;
+            for (int i = 0; i < nodes; i++)
+            {
+                if (temp->next == tail)
+                {
+                    tail = temp;
+                    continue;
+                }
+                temp = 
+            }
+        }
+        
+        T get(int position){
+            if (position >= nodes || position < 0){
+                throw "Pos..";
+            }
+            int index = 0;
+            Node<T>* temp = head;
+            while(temp){
+                if (index = position){
+                    return temp->data
+                }
+                temp = temp->next;
+                index++;
+            }
+        }
+
+        void concat(List<T> &other){
+            tail->next = other->head;
+            nodes += other->nodes; 
+        }
+
+        bool empty(){
+            return head == NULL;
+        }
+
+        int size(){
+            return nodes;
+        }
+
+        void print(){
+            Node<T> *temp = head;
+            while(temp)
+            {
+                cout<< temp->data << endl;
+                temp = temp->next;
+            }
+        }
+
+        void print_reverse(){
+            if (!head){
+                throw "Lista vacia";
+            }
+            head->printReverse();
+        }
+        
+        void clear(){
+        	head->killSelf();
+        }
+
+        Iterator<T> begin(){
+
+        }
+
+        Iterator<T> end(){
+
+        }
 
         ~List();
-};
-
 #endif
